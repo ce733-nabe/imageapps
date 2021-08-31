83639efea59d5300b29cff628f7c5d8d534ae984
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-08-31 17:02:04 +0900

    v2_12

--- imageapps/settings.py
+++ imageapps/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-f not DEBUG:
+if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku #追加
     django_heroku.settings(locals()) #追加
