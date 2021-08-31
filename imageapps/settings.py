df235fc8ad001420d398905081347eba43fe2258
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-08-31 16:10:01 +0900

    v2_7

--- imageapps/settings.py
+++ imageapps/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/css/')]
 print(STATICFILES_DIRS)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
