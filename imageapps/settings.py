a0a179cd314fb4be7a32cf2346460ec3c36c845c
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-09-01 16:32:50 +0900

    v1_8

--- imageapps/settings.py
+++ imageapps/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/css/')]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+print(STATICFILES_DIRS)
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
