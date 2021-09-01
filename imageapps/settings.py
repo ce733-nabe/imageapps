c397790f0399a7608c3fd0ad53d9a1493e5316bb
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-09-01 13:57:20 +0900

    v1

--- imageapps/settings.py
+++ imageapps/settings.py
@@ -21,15 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xr3s@yo!3ipeuculyn$o=58o$mv9#a2eq4ch(@6c(8@$uo55jx'
+#SECRET_KEY = 'django-insecure-xr3s@yo!3ipeuculyn$o=58o$mv9#a2eq4ch(@6c(8@$uo55jx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['imageai-20210831.herokuapp.com']
-#ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+#ALLOWED_HOSTS = ['imageai-20210831.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 #ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -137,7 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 import dj_database_url
-db_from_env = dj_database_url.config()
+#db_from_env = dj_database_url.config()
+#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 
@@ -147,8 +149,6 @@ except ImportError:
     pass
 
 if not DEBUG:
-    #SECRET_KEY = os.environ['SECRET_KEY']
-    #SECRET_KEY ='django-insecure-xr3s@yo!3ipeuculyn$o=58o$mv9#a2eq4ch(@6c(8@$uo55jx'
-    #print(SECRET_KEY)
+    SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
     django_heroku.settings(locals())
